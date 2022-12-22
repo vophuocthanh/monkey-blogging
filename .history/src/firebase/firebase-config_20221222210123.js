a80c27db6226504912f35983a92a0da665135257
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2022-12-22 21:17:20 +0700

    create app

---
+++ .history/src/firebase/firebase-config_20221222210123.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-UGXhKO9d70dBrcvY4fXZZRDZBziND20",
+  authDomain: "monkey-blogging-b0a35.firebaseapp.com",
+  projectId: "monkey-blogging-b0a35",
+  storageBucket: "monkey-blogging-b0a35.appspot.com",
+  messagingSenderId: "711450377166",
+  appId: "1:711450377166:web:159e707833f5c9fb5a099f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ .history/src/firebase/firebase-config_20221222211135.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyA-UGXhKO9d70dBrcvY4fXZZRDZBziND20",
+  authDomain: "monkey-blogging-b0a35.firebaseapp.com",
+  projectId: "monkey-blogging-b0a35",
+  storageBucket: "monkey-blogging-b0a35.appspot.com",
+  messagingSenderId: "711450377166",
+  appId: "1:711450377166:web:159e707833f5c9fb5a099f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
